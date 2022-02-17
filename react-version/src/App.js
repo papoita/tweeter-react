8dc62b63cdcff559b6cee28dbbe16ee3f0f20fd7
PaolaP <paolaperez8@gmail.com>
2022-02-17 17:32:57 -0500

    import and show components in app.js

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation'
+import Profile from './components/Profile'
+import TweetList from './components/TweetList'
+import TweetForm from './components/TweetForm'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation /> 
+      <Profile /> 
+      <TweetList /> 
+      <TweetForm /> 
     </div>
   );
 }
